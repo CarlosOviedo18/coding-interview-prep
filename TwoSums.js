ec09014d566df19e730715bb515e077192d387f6
Carlos <caantoni24@gmail.com>
2026-03-21 00:31:41 -0600

    "more problems"

--- TwoSums.js
+++ TwoSums.js
@@ -12,42 +12,60 @@
 // Salida:
 // [0, 1]
 
+// const twoSums = (nums, target) => {
+//   const map = new Map();
+
+//   for (const [indice, valor] of nums.entries()) {
+//     const findNumber = target - valor;
+
+//     if (map.has(findNumber)) {
+//       return [map.get(findNumber), indice];
+//     } else {
+//       map.set(valor, indice);
+//     }
+//   }
+// };
+
+// console.log(twoSums([7, 2, 11, 15], 9));
+
+
+// PROBLEMA:Dado un array de números, determinar si contiene algún duplicado.
+//Devolver true si hay al menos un valor repetido, false si todos son únicos.
 
 
 
-// Intenta resolverlo solo con esta pista:
+// const duplicadeNumber = (array) => {
+//     const set = new Set();
 
-// “Mientras recorres el array, guarda en un Map lo que ya viste”
-// Pregúntate en cada paso:
-// ¿Qué necesito guardar en el Map?
-// ¿Qué estoy buscando?
-// Si te atoras → vienes y me dices:
+//     for (const valor of array){
+//         if(set.has(valor)){
+//             return true
+//         } 
+//             set.add(valor) 
+//     }
+//     return false 
 
-// 💡 Mini pista (sin spoilear la solución)
-// En Two Sum siempre hay una idea clave:
-// “Si tengo un número… ¿qué número me falta para llegar al target?”
-// Eso es TODO el truco. El Map solo sirve para recordar lo que ya viste.
 
-const twoSums = (nums, target) => {
+// };
 
-  const map = new Map();
-    const total = 0;
+// console.log(duplicadeNumber([2, 3, 5, 6, 56, 1]))
 
-  for (const i of nums) {
+// PROBLEMA:Dado un array de letras, contar cuántas veces aparece cada una.
+//Devolver un Map con cada letra como clave y su cantidad como valor.
 
-    for (const j of nums) {
-        
-           if(map.has(nums)){
-            
-             map.set( total, map.get(i + j));
+const countLetter = (array) => {
+    const map = new Map();
+    for(elem of array){
 
-           }
-
-           if(total === target){
-            console.log(target, "=" ,total)
-           }
+        if(map.has(elem)){
+            map.set(elem, map.get(elem) + 1)
+        }else {
+            map.set(elem, + 1)
+        } 
     }
+return map
 
-  }
-};
-twoSums([2, 7, 11, 15], 9);
+}
+
+
+console.log(countLetter(["a", "b", "a", "c", "b"]));
